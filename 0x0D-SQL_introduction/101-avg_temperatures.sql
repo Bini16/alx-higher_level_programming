c668ecd093d8d2bad9cce743ffe12683973c49d7
Bini16 <abrahambiniyam6@gmail.com>
2023-10-18 00:52:00 +0000

    SQL introduction updated tasks

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- That displays the average temperature (in Fahrenheit) and by city ordered by descending temperature.
+SELECT `city`, AVG(`value`) AS `avg_temp`
+FROM `temperatures`
+GROUP BY `city`
+ORDER BY `avg_temp` DESC;
---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,5 @@
+-- That lists the number of records with the same score in the table second_table and Records are ordered by descending count.
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `score` DESC;
