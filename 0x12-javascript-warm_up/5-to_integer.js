bf9beb482a5e2901f8cbfcd49a8c59327709baf4
Bini16 <abrahambiniyam6@gmail.com>
2023-11-13 16:36:40 +0000

    java script warm up updated tasks

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const convert = Number(process.argv[2]);
+if (isNaN(convert)) 
+{
+  console.log('Not a number');
+} else 
+{
+  console.log('My number: ' + convert);
+}
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+const prog = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < prog.length; i++) 
+{
+  console.log(prog[i]);
+}
